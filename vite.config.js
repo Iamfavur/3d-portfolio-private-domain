42909038cc71a3907e54fd162296bbdf76e0fa73
mac <mac@omnihoeliests-macbook.local>
2024-09-11 23:51:31 +0100

    base path reseted

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3d-portfolio-private-domain/", // YOUR REPO NAME HERE
+  base: "/", // YOUR REPO NAME HERE
   plugins: [react()],
 });
